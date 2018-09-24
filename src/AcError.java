dde930aed87d2bce1da1f9109cef632ff61baee5
nLaugas <nicolasdaniellaugas@gmail.com>
2018-09-24 12:07:32 -0300

    clases realmente modificada

--- src/AcError.java
+++
@@ -1,9 +0,0 @@
-public class AcError extends AcSemantica {
-
-    public AcError(){
-        super(-1);
-    }
-    public void ejecutar(){
-        System.out.println("hay error, en linea.. tal"+" codigo: "+this.getCodigo());
-    }
-}
