0c8aac7f2fb313a0681d6e24d51c546aed22ce89
nLaugas <nicolasdaniellaugas@gmail.com>
2018-09-30 22:41:14 -0300

    interfaz mas agradable

--- src/App.java
+++ src/App.java
@@ -37,6 +37,84 @@ public class App extends JFrame{
     private JPanel a;
     String archivo="";
     private Errors errors;
+    //este metodo es solo para mostrar el contenido de manera prolija
+    //al momento de apretar el boton "token"
+    public String mostrarToken(int valor){
+
+        if (valor == 0){
+            return "EOF";
+        }
+        else if (valor == Parser.ID){
+            return "IDENTIFICADOR";
+        }
+        else if (valor == Parser.ENTERO){
+            return "ENTERO";
+        }
+        else if (valor == Parser.FLOTANTE){
+            return "FLOTANTE";
+        }
+        else if (valor == Parser.CADENA){
+            return "CADENA DE CARACTER";
+        }
+        else if (valor == Parser.ASIG){
+            return "ASIGNACION";
+        }
+        else if (valor == Parser.ID) {
+            return "IDENTIFICADOR";
+        }
+
+
+
+        else if (valor == Parser.ELSE) {
+            return "PALABRA RESERVADA ELSE";
+        }
+
+        else if (valor == Parser.END_IF) {
+            return "PALABRA RESERVADA END_IF";
+        }
+        else if (valor == Parser.IF) {
+            return "PALABRA RESERVADA IF";
+        }
+        else if (valor == Parser.INTEGER) {
+            return "PALABRA RESERVADA INTEGER";
+        }
+        else if (valor == Parser.LET) {
+            return "PALABRA RESERVADA LET";
+        }
+
+        else if (valor == Parser.MUT) {
+            return "PALABRA RESERVADA MUT";
+        }
+
+        else if (valor == Parser.LOOP) {
+            return "PALABRA RESERVADA LOOP";
+        }
+        else if (valor == Parser.UNTIL) {
+            return "PALABRA RESERVADA UNTIL";
+        }
+
+        else if (valor == Parser.PRINT) {
+            return "PALABRA RESERVADA PRINT";
+        }
+
+        else if (valor == Parser.SINGLE) {
+            return "PALABRA RESERVADA SINGLE";
+        }
+        else if (valor == Parser.MAYIG) {
+            return "OPERADOR MAYOR IGUAL";
+        }
+        else if (valor == Parser.MENIG) {
+            return "OPERADOR MENOR IGUAL";
+        }
+        else if (valor == Parser.DIST) {
+            return "DISTINTO";
+        }
+
+        else if (valor < 255) {
+            return "SIMBOLO "+(char)valor;
+        }
+        return "";
+    }
 
     public App()  throws IOException {
         FileReader file = new FileReader("src/srcCode");
@@ -53,12 +131,16 @@ public class App extends JFrame{
         Parser par = new Parser(lexical,st,errors);
 
         add(panel1);
-        setSize(400,500);
+        setSize(700,500);
 
+
+        //al presionar compila y consume todos los token
         compilarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 par.run();
+                textArea1.removeAll();
+                textArea3.removeAll();
                 textArea3.append("ERRORES \n \n");
                 while (!errors.isEmpty()) {
                     textArea3.append(errors.getError() + " fila " + errors.getRow() + " columna " + errors.getColumn() + "\n");
@@ -67,12 +149,17 @@ public class App extends JFrame{
                 while (!st.isEmpty()) {
                     textArea1.append(st.getAtributosNextLexema() + "\n");
                 }
+                compilarButton.setEnabled(false);
+                tokenButton.setEnabled(false);
 
             }
+
         });
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //al presionar va retornando token y mostrandolos para probar solo
+        //el analizador lexico
         tokenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -80,13 +167,13 @@ public class App extends JFrame{
                 if (token != -1) {
                     if (token == 0)
                         textArea1.append("fin de archivo\n");
-                    textArea1.append(String.valueOf(token)+"\n");
+                    textArea1.append(mostrarToken(token)+"\n");
                 }
                 if (!errors.isEmpty()) {
                     textArea3.append(errors.getError() + " fila " + errors.getRow() + " columna " + errors.getColumn() + "\n");
                 }
 
-
+                compilarButton.setEnabled(false);
             }
         });
 
@@ -100,6 +187,4 @@ public class App extends JFrame{
             }
         });
     }
-
-
 }
