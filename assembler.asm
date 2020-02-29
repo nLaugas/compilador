947a64527a10519f0d715909c615c77df476b5df
LucasTelesco <telescolucas19@gmail.com>
2020-02-28 21:26:34 -0300

    corrigue reduccion simple

--- assembler.asm
+++ assembler.asm
@@ -20,125 +20,30 @@ _@min_float2 DQ -3.40282347e-38
 _@max_integer DD 32768
 _@min_integer DD -32768
 _memaux DW ?
-_@AUX5 DD ?
 _finalizacion_de_programa DB "finalizacion de programa" ,0
-_@AUX4 DD ?
-_@AUX3 DD ?
-_@AUX2 DD ?
 _@AUX1 DD ?
 _@AUX0 DD ?
+_2000000p0 DD 2000000.0
 _f DD ?
+_1000p0 DD 1000.0
 _e DD ?
 _d DD ?
-_1000p0 DD 1000.0
-_@AUX12 DD ?
 _c DD ?
-_@AUX11 DD ?
 _b DD ?
-_@AUX10 DD ?
 _a DD ?
-_2000p0 DD 2000.0
-_es_distinto DB "es distinto" ,0
-_es_igual DB "es igual" ,0
-_5000p0 DD 5000.0
 _x DD ?
-_@AUX9 DD ?
-_@AUX8 DD ?
-_@AUX7 DD ?
-_@AUX6 DD ?
+_2000p0 DD 2000.0
 .CODE
 START:
 
-FLD _1000p0
+FILD _1000p0
+FMUL _2000p0
+FSTP _@AUX0
+
+
+
+FLD _2000000p0
 FSTP _e
-
-FLD _2000p0
-FSTP _b
-
-FLD _1000p0
-FSTP _d
-
-FILD _e
-FMUL _d
-FSTP _@AUX3
-
-
-
-FLD _@AUX3
-FLD _b
-FADD
-JO @OVERFLOW_EN_SUMA
-FSTP _@AUX4
-
-
-
-FLD _@AUX4
-FSTP _x
-
-FLD _@AUX3
-FLD _b
-FADD
-JO @OVERFLOW_EN_SUMA
-FSTP _@AUX7
-
-
-
-FLD _@AUX7
-FSTP _f
-
-FLD _x
-
-FCOMP _f
-
-FSTSW _memaux
-
-MOV AX, _memaux
-
-SAHF
-JNE  Label13
-
-invoke MessageBox, NULL, addr _es_igual, addr TITULO , MB_OK 
-JMP Label14
-Label13:
-
-invoke MessageBox, NULL, addr _es_distinto, addr TITULO , MB_OK 
-
-Label14:
-
-
-FLD _5000p0
-FSTP _b
-
-FLD _@AUX6
-FLD _b
-FADD
-JO @OVERFLOW_EN_SUMA
-FSTP _@AUX11
-
-
-
-FLD _@AUX11
-FSTP _x
-
-FLD _x
-
-FCOMP _f
-
-FSTSW _memaux
-
-MOV AX, _memaux
-
-SAHF
-JNE  Label23
-
-invoke MessageBox, NULL, addr _es_igual, addr TITULO , MB_OK 
-JMP Label24
-Label23:
-
-invoke MessageBox, NULL, addr _es_distinto, addr TITULO , MB_OK 
-
-Label24:
-
 
 invoke MessageBox, NULL, addr _finalizacion_de_programa, addr TITULO , MB_OK 
 jMP EXIT
